d22cfb7b6b32d73eba1d49b97e106a5ef92ba430
Miguel Marinhas <miguel.marinhas@gmail.com>
2012-04-11 19:44:59 +0100

    Categories

---
+++ _plugins/site_process.rb
@@ -0,0 +1,18 @@
+module Jekyll
+  class Site
+    def process
+      self.reset
+      self.read
+      self.generate
+      self.render
+
+      # these must come after render
+      self.generate_tags_categories
+      #self.generate_archives
+      #self.generate_projects
+
+      self.cleanup
+      self.write
+    end
+  end
+end
